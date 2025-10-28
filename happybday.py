027de75dcc9cde45a820adfd419a354831c3b185
manulapulga <manupoolakkalgtalk@gmail.com>
2025-10-28 15:10:32 +0530

    Auto commit 28-10-2025 15:10:32.52

--- happybday.py
+++ happybday.py
@@ -151,7 +151,7 @@ def birthday_card_page():
         <div class="balloons">🎁✨🎊</div>
         <div class="birthday-message">
             Wishing you a day as wonderful and special as you are!<br>
-            May your birthday be as special as the secret hidden down there, waiting for your gentle touch!
+            May your birthday be as special as the secret hidden down here, waiting for your gentle touch!
         </div>
         <div class="balloons">💫🌟❤️</div>
     </div>
